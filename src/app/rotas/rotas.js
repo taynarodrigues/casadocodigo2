7b35261a05e866f5116df327b7fb6bd38ba33daa
Tayna Rodrigues <taynajesus@gmail.com>
2021-06-07 22:28:27 -0400

    Aplicação, Validação e tratamento de Erros, autenticação,acesso usuário

---
+++ src/app/rotas/rotas.js
@@ -0,0 +1,8 @@
+const livroRotas = require('./livro-rotas');
+const baseRotas = require('./base-rotas');
+
+module.exports = (app) => {
+    baseRotas(app);
+    livroRotas(app);
+};
+
---
+++ src/app/views/livros/index.js
@@ -0,0 +1,5 @@
+module.exports = {
+    // lista: require('./lista/lista.marko'),
+    lista: require('./lista/lista.marko'),
+    form: require('./form/form.marko')
+}
